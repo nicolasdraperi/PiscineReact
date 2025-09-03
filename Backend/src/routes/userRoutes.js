4dffa7b1c159169744d3bdab26a82c30d866ad41
nicolasdraperi <nicolas.draperi2@gmail.com>
2025-09-03 12:41:43 +0200

    Ajout route pour le profil

--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const Photo = require("../models/Photo"); 
 const authMiddleware = require("../middleware/authMiddleware");
+
 
 router.get("/me", authMiddleware, async (req, res) => {
   try {
-    const user = await User.findById(req.user.id).select("-password"); // enlever le mot de passe
-    res.json(user);
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) return res.status(404).json({ message: "Utilisateur introuvable" });
+    const photos = await Photo.find({ userId: req.user.id }).sort({ date: -1 });
+    const lastPhoto = photos.length > 0 ? photos[0] : null;
+
+    res.json({
+      user,
+      photos,
+      lastPhoto
+    });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
 
 module.exports = router;
+
+
